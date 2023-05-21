46e8da8a25f1f882e89d1c1f3c0e3e86878a0144
Iker Ordoñez <iker.o.f@opendeusto.es>
2023-05-21 17:08:41 +0200

    Inicializacion del proyecto

---
+++ GestionHotelesCliente/Clases/Persona.cpp
@@ -0,0 +1,8 @@
+/*
+ * Persona.cpp
+ *
+ *  Created on: 21 may 2023
+ *      Author: iker2
+ */
+
+
---
+++ GestionHotelesCliente/Clases/Persona.h
@@ -0,0 +1,13 @@
+/*
+ * Persona.h
+ *
+ *  Created on: 21 may 2023
+ *      Author: iker2
+ */
+
+#ifndef CLASES_PERSONA_H_
+#define CLASES_PERSONA_H_
+
+
+
+#endif /* CLASES_PERSONA_H_ */
